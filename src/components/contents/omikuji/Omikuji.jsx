1562d3980ff61aafc145f8f69e39d6829573115f
matuiti <ichi.animo@gmail.com>
2024-03-24 15:40:14 +0900

    微調整

--- src/components/contents/omikuji/Omikuji.jsx
+++ src/components/contents/omikuji/Omikuji.jsx
@@ -17,7 +17,7 @@ const Omikuji = () => {
   return (
     <div className={`text-center flex flex-col gap-20 pb-6 bg-gray-300 m-auto shadow-md md:w-1/2`}>
       <h1 className={`font-bold text-3xl bg-blue-500 py-4 shadow-md`}>運勢を占う</h1>
-      <p className={`${currentOmikuji === "大吉" ? "text-red-600 font-bold" : "text-black"} bg-yellow-200 m-auto py-10 px-10 text-6xl shadow-md`}>{currentOmikuji ? currentOmikuji : "^=^b"}</p>
+      <p className={`${currentOmikuji === "大吉" ? "text-red-600 font-bold" : "text-black"} bg-yellow-200 m-auto py-10 px-10 text-6xl shadow-md w-80`}>{currentOmikuji ? currentOmikuji : "^=^b"}</p>
       <button className={`shadow-md pt-1 pb-1 pl-4 pr-4 mx-auto -mt-8 bg-green-700 text-white hover:cursor-pointer hover:transition-all rounded-md`} onClick={handleClick}>おみくじを引く</button>
     </div>
   );
