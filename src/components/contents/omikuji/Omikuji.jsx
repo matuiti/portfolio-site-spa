0b7764179309341bfc85c9f203c16b9f84c6fbf3
matuiti <ichi.animo@gmail.com>
2024-03-24 22:44:40 +0900

    スタイリング

--- src/components/contents/omikuji/Omikuji.jsx
+++ src/components/contents/omikuji/Omikuji.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 const Omikuji = () => {
   const [isDrawing, setIsDrawing] = useState(false);
   const [omikujiResult, setOmikujiResult] = useState(null);
   const [isResultPlaying, setIsResultPlaying] = useState(false); // おみくじの結果の再生状態
   const videoRef = useRef(null); // ビデオ要素の参照
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     if (isDrawing && !isResultPlaying) {
@@ -14,7 +15,7 @@ const Omikuji = () => {
         waitingVideo.play();
         waitingVideo.onended = () => {
           const min = 1; // waiting.mp4を除外するために1から始める
-          const max = 2; // ビデオファイル名の数
+          const max = 3; // ビデオファイル名の数
           const rand = Math.floor(Math.random() * (max - min + 1)) + min;
           if (rand === 1) {
             setOmikujiResult("daikiti.mp4");
@@ -29,21 +30,35 @@ const Omikuji = () => {
   }, [isDrawing, isResultPlaying]);
 
   const handleClick = () => {
-    if (!omikujiResult && !isResultPlaying) { // おみくじ結果が表示されていないかつ待ち動画が再生中でない場合のみ処理を実行
+    setIsClicked(true)
+    if (!omikujiResult && !isResultPlaying) {
+      // おみくじ結果が表示されていないかつ待ち動画が再生中でない場合のみ処理を実行
       setIsDrawing(true);
-    } else if (omikujiResult) { // おみくじ結果が表示されている場合は再度おみくじを引く
+    } else if (omikujiResult) {
+      // おみくじ結果が表示されている場合は再度おみくじを引く
       setOmikujiResult(null);
       setIsResultPlaying(false);
     }
   };
 
   return (
-    <div className={`text-center flex flex-col gap-20 pb-6 bg-gray-300 m-auto shadow-md md:w-1/2`}>
-      <h1 className={`font-bold text-3xl bg-blue-500 py-4 shadow-md`}>運勢を占う</h1>
-      <button onClick={handleClick} disabled={isDrawing || isResultPlaying}> {/* 待ち動画が再生中かつおみくじの結果が表示されていない場合はボタンを無効に */}
-        {omikujiResult ? "もう一度引く" : "おみくじを引く"}
-      </button>
-      <div className="media-container">
+    <div
+      style={{ width: "375px" }}
+      className={`h-screen m-auto text-center pb-6 bg-gray-300 shadow-md`}
+    >
+      <h1
+        className={`mb-10 font-bold shadow-white text-3xl bg-purple-500 py-4 shadow-md`}
+      >
+        運勢を占う
+      </h1>
+      <div className="w-1/2 m-auto">
+        {!isClicked && (
+          <p
+            className={` bg-yellow-200 m-auto py-10 px-10 text-6xl shadow-md`}
+          >
+            ^=^b
+          </p>
+        )}
         {isDrawing && (
           <video autoPlay ref={videoRef}>
             <source src="public/images/omikuji/waiting.mp4" type="video/mp4" />
@@ -51,8 +66,12 @@ const Omikuji = () => {
           </video>
         )}
         {omikujiResult && !isDrawing && (
-          <video autoPlay onEnded={() => setIsResultPlaying(false)}> {/* おみくじの結果の再生が終了したらisResultPlayingをfalseに設定 */}
-            <source src={`public/images/omikuji/${omikujiResult}`} type="video/mp4" />
+          <video autoPlay onEnded={() => setIsResultPlaying(false)}>
+            {/* おみくじの結果の再生が終了したらisResultPlayingをfalseに設定 */}
+            <source
+              src={`public/images/omikuji/${omikujiResult}`}
+              type="video/mp4"
+            />
             Your browser does not support the video tag.
           </video>
         )}
@@ -61,9 +80,14 @@ const Omikuji = () => {
           <img src={`public/images/omikuji/${omikujiResult}`} alt="omikuji result" />
         )} */}
       </div>
-      {omikujiResult && (
-        <h2>結果: {omikujiResult}</h2>
-      )}
+      <button
+        onClick={handleClick}
+        disabled={isDrawing || isResultPlaying}
+        className={`shadow-md pt-1 pb-1 px-8 mx-auto mt-10 bg-green-700 text-white hover:cursor-pointer hover:transition-all rounded-md`}
+      >
+        {/* 待ち動画が再生中かつおみくじの結果が表示されていない場合はボタンを無効に */}
+        {omikujiResult ? "もう一度引く" : "おみくじを引く"}
+      </button>
     </div>
   );
 };
