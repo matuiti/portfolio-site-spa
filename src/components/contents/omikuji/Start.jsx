91c765961f711c137b9a79886b6f4fe46614ebd4
matuiti <ichi.animo@gmail.com>
2024-03-29 13:51:46 +0900

    startの画面実装、ボタンの機能は未実装

--- src/components/contents/omikuji/Start.jsx
+++ src/components/contents/omikuji/Start.jsx
@@ -1,7 +1,20 @@
-
 function Start() {
   return (
-    <div>Start</div>
+    <div className="relative flex justify-center items-center">
+      <img src="/images/omikuji/start.png" />
+      <button style={{ top:"570px", fontSize:"25px", backgroundColor:"#D90000",width:"279px",height:"66px"}} className="absolute flex justify-center items-center text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+      おみくじを引く
+      <svg style={{marginLeft:"8px", marginTop:"7px",width:"35px",height:"35px",viewBox:"0 0 35.009 32.023"}}>
+        <g transform="matrix(0.966, -0.259, 0.259, 0.966, -7.716, 5.866)">
+          <path id="パス_1" data-name="パス 1" d="M171.236,8.607c1.773.649,3.9-.716,4.756-3.048S176.1.81,174.327.161s-3.9.716-4.756,3.049S169.463,7.959,171.236,8.607Z" transform="translate(-153.68 0)" fill="#fff"/>
+          <path id="パス_2" data-name="パス 2" d="M22.144,70.755c1.855-.161,3.187-2.269,2.976-4.709s-1.886-4.288-3.741-4.128-3.187,2.269-2.976,4.71S20.289,70.915,22.144,70.755Z" transform="translate(-11.325 -58.428)" fill="#fff"/>
+          <path id="パス_3" data-name="パス 3" d="M317.9,81.389c1.628.957,3.967,0,5.225-2.145s.958-4.653-.67-5.61-3.968.005-5.225,2.147S316.268,80.433,317.9,81.389Z" transform="translate(-292.633 -69.144)" fill="#fff"/>
+          <path id="パス_4" data-name="パス 4" d="M366.722,242.23c-1.055-1.533-3.546-1.652-5.564-.264s-2.8,3.757-1.743,5.291,3.546,1.652,5.564.263S367.777,243.764,366.722,242.23Z" transform="translate(-332.739 -227.447)" fill="#fff"/>
+          <path id="パス_5" data-name="パス 5" d="M23.145,205.7a4.288,4.288,0,0,0-6.511-3.073c-1.33.807-2.18,1.861-3.325,2.322s-4.352-.911-6.349,2.715a5.184,5.184,0,0,0,2.528,7.046c2.01.948,3.6.842,4.768,1.393s2.1,1.846,4.106,2.794a5.184,5.184,0,0,0,7.046-2.528c1.53-3.847-1.569-5.45-1.941-6.627S23.367,207.237,23.145,205.7Z" transform="translate(0 -190.626)" fill="#fff"/>
+        </g>
+      </svg>
+    </button>
+    </div>
   )
 }
 
