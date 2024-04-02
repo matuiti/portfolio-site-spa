bee91f2b264923ae9945c1e6dd9332496d4fe982
matuiti <ichi.animo@gmail.com>
2024-04-02 20:11:36 +0900

    navをflex-endで右寄せに

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
     <header className="flex justify-between cursor-pointer w-full pt-1 pb-2 px-4 text-md tracking-wide text-neutral-400 items-center bg-gray-700 shadow-sm">
       <Link to="/"><h1 className="italic text-gray-400 mr-4 hover:text-white">Portfolio</h1></Link>
       <nav>
-        <ul className="flex flex-wrap gap-x-3 gap-y-2 text-sm">
+        <ul className="flex justify-end flex-wrap gap-x-3 gap-y-2 text-sm">
           <Link to="/sarugame"><li className="hover:text-white">サルゲーム</li></Link>
-          <Link to="/omikuji"><li className="hover:text-white">猫みくじ</li></Link>
           <Link to="/stopwatch"><li className="hover:text-white">ストップウォッチ</li></Link>
+          <Link to="/omikuji"><li className="hover:text-white">猫みくじ</li></Link>
           <Link to="/yudetamagotimer"><li className="hover:text-white">ゆでたまごタイマー</li></Link>
         </ul>
       </nav>
